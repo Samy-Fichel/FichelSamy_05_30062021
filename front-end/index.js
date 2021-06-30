b5d9c6c9e985927e354c2f8ebb8d382fddcf0524
Fichel <samyfichel@gmail.com>
2021-06-30 20:26:33 +0200

    ajout du fichier index.js

---
+++ front-end/index.js
@@ -0,0 +1,83 @@
+//Récuperer les ours sur le serveur
+function getServerTeddies() {
+    const teddies = [
+        {
+          "colors": ["Tan", "Chocolate", "Black", "White"],
+          "_id": "5be9c8541c9d440000665243",
+          "name": "Norbert",
+          "price": 2900,
+          "imageUrl": "teddy_1.jpg",
+          "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+        },
+        {
+          "colors": [
+            "Pale brown",
+            "Dark brown",
+            "White"
+          ],
+          "_id": "5beaa8bf1c9d440000a57d94",
+          "name": "Arnold",
+          "price": 3900,
+          "imageUrl": "teddy_2.jpg",
+          "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+        },
+        {
+          "colors": [
+            "Brown"
+          ],
+          "_id": "5beaaa8f1c9d440000a57d95",
+          "name": "Lenny and Carl",
+          "price": 5900,
+          "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+          "imageUrl": "teddy_3.jpg"
+        },
+        {
+          "colors": [
+            "Brown",
+            "Blue",
+            "Pink"
+          ],
+          "_id": "5beaabe91c9d440000a57d96",
+          "name": "Gustav",
+          "price": 4500,
+          "imageUrl": "teddy_4.jpg",
+          "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+        },
+        {
+          "colors": [
+            "Beige",
+            "Tan",
+            "Chocolate"
+          ],
+          "_id": "5beaacd41c9d440000a57d97",
+          "name": "Garfunkel",
+          "price": 5500,
+          "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+          "imageUrl": "teddy_5.jpg"
+        }
+      ];
+
+    return teddies;
+}
+
+//Afficher un ours dans le HTML 
+function displayHTMLTeddies(teddies){
+    //Modifier HTML grâce à la variable "teddy"
+    for (let i = 0; i < teddies.length; i++){
+        //i = 2
+        const teddy = teddies[i];
+
+        console.log ("name: ", teddy);
+        const app = document.getElementById("app");
+        console.log("Le Div dans le HTML", app);
+    app.innerHTML = teddy.name;
+    }
+    return null;
+}
+//On appelle notre première fonction
+// Et on stocke le "return" de cette fonction dans la variable "myVariableTeddies"
+const myVariableTeddies = getServerTeddies();
+console.log(myVariableTeddies);
+
+//On appelle notre 2ème fonction
+displayHTMLTeddies(myVariableTeddies); 
